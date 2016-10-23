2e499d0a0dc6f6c5570d342c2e7e90452d4cb5ff
Tom Hoover <tom@hisword.net>
2016-10-22 21:59:43 -0500

    rename ddns to ddns.sh

--- afterUnRAIDreboot.sh
+++ afterUnRAIDreboot.sh
@@ -8,5 +8,5 @@ ssh root@unraid 'cat .ssh/authorized_keys'
 scp ~/.boto root@unraid:
 ssh root@unraid 'mkdir -p Dropbox'
 ssh root@unraid 'curl -L https://github.com/sequenceiq/docker-alpine-dig/releases/download/v9.10.2/dig.tgz|tar -xzv -C /usr/local/bin/'
-scp ~/bin/ddns root@unraid:/etc/cron.hourly
+scp ~/bin/ddns.sh root@unraid:/etc/cron.hourly
 ssh root@unraid 'ls -l /etc/cron.hourly'
