2d62983bee28f7297e4790ae76e29fa0f5e122a3
Tom Hoover <tom@hisword.net>
2016-11-12 16:23:03 -0600

    modify to bootstrap my 'unraid' media server

--- afterUnRAIDreboot.sh
+++ afterUnRAIDreboot.sh
@@ -10,3 +10,5 @@ ssh root@unraid 'mkdir -p Dropbox'
 ssh root@unraid 'curl -L https://github.com/sequenceiq/docker-alpine-dig/releases/download/v9.10.2/dig.tgz|tar -xzv -C /usr/local/bin/'
 scp ~/bin/ddns.sh root@unraid:/etc/cron.hourly
 ssh root@unraid 'ls -l /etc/cron.hourly'
+scp ~/src/bootstrap/bootstrap.sh root@unraid:
+echo "ssh to unraid and execute bootstrap.sh"
