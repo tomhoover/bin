5e52827da058b971ff77a4dd8a067ecff108b5f1
Tom Hoover <tom@hisword.net>
2016-11-13 19:48:45 -0600

    weekly backup listing of installed homebrew apps

---
+++ homebrewBackup_weekly.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# shellcheck disable=SC1090
+. "$HOME/.keychain/$HOSTNAME-sh"
+/usr/local/bin/brew list > ~/.config/homebrew/brew.installed
+/usr/local/bin/brew cask list > ~/.config/homebrew/cask.installed
+/usr/local/bin/vcsh homebrew add ~/.config/homebrew/*
+/usr/local/bin/vcsh homebrew commit -m "$(date)"
+/usr/local/bin/vcsh homebrew push
