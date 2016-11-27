1b16325dcf9f27e074a0e2349c64711e4b92d814
Tom Hoover <tom@hisword.net>
2016-11-26 20:50:43 -0600

    created shared github_rsa ssh ID & misc cleanup

--- afterUnRAIDreboot.sh
+++ afterUnRAIDreboot.sh
@@ -3,15 +3,28 @@
 # Exit immediately on error.
 set -e
 
-#ssh root@unraid echo "export TERM=linux" >> .bash_profile
-ssh-copy-id root@unraid
+echo "===== copy authorized_keys, config & github_rsa to .ssh"
+ssh-copy-id -i ~/.ssh/id_rsa.pub root@unraid
 ssh root@unraid 'cat .ssh/authorized_keys'
+scp ~/.ssh/config root@unraid:.ssh
+scp ~/.ssh/github_rsa root@unraid:.ssh
+ssh root@unraid 'ls -al .ssh'
+echo ""
 
-#ssh root@unraid mkdir -p bin
-scp ~/.boto root@unraid:
-ssh root@unraid 'mkdir -p Dropbox'
+echo "===== download dig"
 ssh root@unraid 'curl -L https://github.com/sequenceiq/docker-alpine-dig/releases/download/v9.10.2/dig.tgz|tar -xzv -C /usr/local/bin/'
+echo ""
+
+echo "===== copy ddns.sh to /etc/cron.hourly"
 scp ~/bin/ddns.sh root@unraid:/etc/cron.hourly
 ssh root@unraid 'ls -l /etc/cron.hourly'
+echo ""
+
+echo "===== copy .boto & bootstrap.sh"
+ssh root@unraid 'mkdir -p Dropbox'
+scp ~/.boto root@unraid:
 scp ~/src/bootstrap/bootstrap.sh root@unraid:
+ssh root@unraid 'ls -al'
+echo ""
+
 echo "ssh to unraid and execute bootstrap.sh"
