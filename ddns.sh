efccdb384e60f56c98b6d680ecdd8ed256e04cbc
Tom Hoover <tom@hisword.net>
2016-12-03 21:25:55 -0600

    incorporate shellcheck recommendations

--- ddns.sh
+++ ddns.sh
@@ -5,23 +5,27 @@ set -e
 
 PATH=$PATH:/usr/local/bin
 FILE="$HOME/Dropbox/lastip"
-LASTIP=`cat $FILE`
-WAN=`dig +short myip.opendns.com @resolver1.opendns.com`
+LASTIP=$(cat "$FILE")
+WAN="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
-if [[ `hostname -s` == "unraid" ]]; then
+if [[ $(hostname -s) == "unraid" ]]; then
+  # shellcheck disable=SC1091
   [[ -f /root/.boto ]] && . /root/.boto
+  # shellcheck disable=SC2154
   CLI53="docker run --rm -e AWS_ACCESS_KEY_ID=${aws_access_key_id} -e AWS_SECRET_ACCESS_KEY=${aws_secret_access_key} tomhoover/docker-cli53"
 else
-  [ `which cli53` ] || brew install cli53
+  [ "$(which cli53)" ] || brew install cli53
   CLI53="cli53"
 fi
 
 if [[ "$LASTIP" != "$WAN" ]]; then
 	echo "IP changed from $LASTIP to $WAN"
-	echo $WAN > $FILE
-#	${CLI53} rrcreate --replace --ttl 300 t0m.us @ A $WAN	# previous command (for python cli53)
-#	${CLI53} rrcreate --replace t0m.us '@ 300 A' ${WAN}	# won't work--space must be inside single quote to work
+	echo "$WAN" > "$FILE"
+
+# shellcheck disable=SC2086
 if [[ "$HOSTNAME" == "unraid" ]]; then
+    #   ${CLI53} rrcreate --replace --ttl 300 t0m.us @ A $WAN	# previous command (for python cli53)
+    #   ${CLI53} rrcreate --replace t0m.us '@ 300 A' ${WAN}	# won't work--space must be inside single quote to work
         ${CLI53} rrcreate --replace t0m.us '@ 300 A '${WAN}
     else
         ${CLI53} rrcreate --replace t0m.us ${HOSTNAME}' 300 A '${WAN}
