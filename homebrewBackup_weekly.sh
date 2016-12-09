22b38446f3e42a6fad48444f5dfc6672ca552a0c
Tom Hoover <tom@hisword.net>
2016-12-09 16:53:23 -0600

    misc shellcheck suggested changes

--- homebrewBackup_weekly.sh
+++ homebrewBackup_weekly.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 # Exit immediately on error.
 set -e
 
 # shellcheck disable=SC1090
-. "$HOME/.keychain/$HOSTNAME-sh"
-/usr/local/bin/brew list > ~/.config/homebrew/brew.installed
-/usr/local/bin/brew cask list > ~/.config/homebrew/cask.installed
+. "$HOME/.keychain/$(hostname -s)-sh"
+/usr/local/bin/brew list > "$HOME/.config/homebrew/brew.installed"
+/usr/local/bin/brew cask list > "$HOME/.config/homebrew/cask.installed"
 /usr/local/bin/brew bundle dump --global --force
-/usr/local/bin/vcsh homebrew add ~/.Brewfile
-/usr/local/bin/vcsh homebrew add ~/.config/homebrew/*
+/usr/local/bin/vcsh homebrew add "$HOME/.Brewfile"
+/usr/local/bin/vcsh homebrew add "$HOME/.config/homebrew/*"
 /usr/local/bin/vcsh homebrew commit -m "$(date)"
 /usr/local/bin/vcsh homebrew push
