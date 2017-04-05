e8ec92a8179c6559eee474bfcdb8d0f39acd68e2
Tom Hoover <tom@hisword.net>
2017-04-05 12:15:05 -0500

    update config-backup.sh to work with ariel

--- config-backup.sh
+++ config-backup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # set -e: exit script immediately upon error
-# set -o pipefail: cause a pipeline to fail, if any command within it fails
-set -e -o pipefail
+set -e
 
 if [ $EUID != 0 ]; then
     sudo "$0" "$@"
@@ -10,20 +9,23 @@ if [ $EUID != 0 ]; then
 fi
 
 if [ "$(hostname -s)" = ariel ]; then
+    mkdir -pm 700 /Users/tom/.config-backup
     CONFIG_DIR=/Users/tom/.config-backup/ariel
 elif [ "$(hostname -s)" = lemuel ]; then
+    mkdir -pm 700 /root/.config-backup
     CONFIG_DIR=/root/.config-backup/lemuel
 else
     exit
 fi
 
-CONFIG_LIST=$CONFIG_DIR/config.txt
+mkdir -p "$CONFIG_DIR" || exit
+cd "$CONFIG_DIR" && git pull --rebase --autostash
 
-mkdir -pm 700 "$CONFIG_DIR" || exit
+CONFIG_LIST=$CONFIG_DIR/config.txt
 
 if [ "$1" != ""  ] ; then echo "$(pwd)/$1" >> "$CONFIG_LIST" ; fi
 sort -uo "$CONFIG_LIST" "$CONFIG_LIST"
 rsync -av --files-from=$CONFIG_LIST / "$CONFIG_DIR/"
-cd "$CONFIG_DIR" && git add . && git commit -m "$(date)"
+cd "$CONFIG_DIR" && git add . && git commit -m "$(date)" && git push
 
-tree -aI .git "$CONFIG_DIR"
+tree -aI .git "$CONFIG_DIR/.."
