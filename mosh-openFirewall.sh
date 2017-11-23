2c36cbe21e587528337ca2ca87217af4a1c016f0
Tom Hoover <tom@hisword.net>
2017-11-23 07:38:54 -0600

    open firewall to allow mosh-server connections

---
+++ mosh-openFirewall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# set -e: exit script immediately upon error
+# set -u: treat unset variables as an error
+# set -o pipefail: cause a pipeline to fail, if any command within it fails
+set -eu -o pipefail
+
+MOSH_SERVER=`ls -l $(which mosh-server) | sed 's|^.*Cellar|/usr/local/Cellar|'`
+
+echo ""
+echo "enter sudo password when prompted:"
+echo ""
+
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --add $MOSH_SERVER
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --unblockapp $MOSH_SERVER
