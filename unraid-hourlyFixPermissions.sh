d6cdff2444190624c7cd5371638146492f0d46db
Tom Hoover <tom@hisword.net>
2017-12-11 18:01:11 -0600

    add unraid-hourlyFixPermissions.sh (to be run via /etc/cron.hourly)

---
+++ unraid-hourlyFixPermissions.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+[ "$(hostname -s)" = unraid  ] || exit
+
+# set -e: exit script immediately upon error
+# set -u: treat unset variables as an error
+# set -o pipefail: cause a pipeline to fail, if any command within it fails
+set -eu -o pipefail
+
+fixPermissions()
+{
+    cd "$1" && pwd
+    find . -type d -exec chmod 775 {} \;
+#    find . -type f -exec chmod 664 {} \;
+#    chown -R nobody:users -- *
+}
+
+#fixPermissions "/mnt/user/Movies"
+#fixPermissions "/mnt/user/TV"
+#fixPermissions "/mnt/user/Media"
+#fixPermissions "/mnt/user/Music"
+#fixPermissions "/mnt/user/Photos"
+fixPermissions "/mnt/user/Media - Originals/MCEB_in/cTivo"
+#fixPermissions "/mnt/user/PlayOn"
