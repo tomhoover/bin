e750c299612fb534ef2b7dedc95636a80d1014b8
Tom Hoover <tom@hisword.net>
2018-01-15 09:04:36 -0600

    fix 'the input device is not a TTY' error

--- unraid-hourlyFixPermissions.sh
+++ unraid-hourlyFixPermissions.sh
@@ -22,4 +22,4 @@ fixPermissions "/mnt/user/Plex"
 fixPermissions "/mnt/user/Media - Originals/MCEB_in/"
 fixPermissions "/mnt/user/PlayOn"
 
-docker exec -it plex ls -l / | grep "drwxrwxrwx.*transcode" || docker exec -it plex chmod 777 /transcode
+#docker exec -it plex ls -l / | grep "drwxrwxrwx.*transcode" || docker exec -it plex chmod 777 /transcode
