8d0cbf0fc5844cc4d0adf00ec812a87100e1a382
Tom Hoover <tom@hisword.net>
2020-06-30 07:18:28 -0500

    duplicacy-backup.sh complete rewrite

--- duplicacy-backup.sh
+++ duplicacy-backup.sh
@@ -1,70 +1,63 @@
-#!/bin/bash
-
-exit
-
+#!/usr/bin/env bash
 # set -e: exit script immediately upon error
 # set -u: treat unset variables as an error
 # set -o pipefail: cause a pipeline to fail, if any command within it fails
 set -eu -o pipefail
 
-[[ -f ${HOME}/.SECRETS ]] && . ${HOME}/.SECRETS
-
-# wasabi://us-west-1@s3.us-west-1.wasabisys.com/bucket/path
-
-backupRepositoryVerbose()
-{
-    echo ""
-    echo ""
-    echo "********** Verbose backup of $1 **********"
-    echo ""
-    cd "$1" && pwd
-    cat .duplicacy/filters
-    $DUPLICACY -v -d backup
-    $DUPLICACY list
-}
+DUPLICACY=duplicacy
 
 backupRepository()
 {
-    echo ""
-    echo ""
-    echo "********** Backup of $1 **********"
-    echo ""
-    cd "$1" && pwd
-    $DUPLICACY backup
-    #$DUPLICACY prune -keep 30:180 -keep 7:30 -keep 1:7
-    # wasabi charges for 90 days minimum, so no financial reason for early pruning of snapshots
-    $DUPLICACY prune -keep 7:90 -keep 30:180 -keep 0:360
-    #$DUPLICACY list
-}
+    cd "$1" || exit
+    chmod -R g=,o= .duplicacy
 
-copyDefault2b2()
-{
-    echo ""
-    echo ""
-    echo "********** Copy $1 to B2 **********"
-    echo ""
-    cd "$1" && pwd
-    $DUPLICACY copy -from default -to b2
-    #$DUPLICACY prune -keep 30:180 -keep 7:30 -keep 1:7
+    DUPLICACY_LOGS=.duplicacy/logs
+    [[ -d $DUPLICACY_LOGS ]] || mkdir -p $DUPLICACY_LOGS
+
+    DATETIME=$(date "+%Y%m%d-%H%M%S")
+
+    echo "# Delete old logs..."
+    find ./$DUPLICACY_LOGS -name "*.log" -type f -mtime +14 -delete
+    echo "# Done"
+
+    echo "# Start Backup..."
+    $DUPLICACY -log backup -stats -threads 2 | tee "$DUPLICACY_LOGS/$DATETIME-backup.log"
+    echo "# Done"
 }
 
 if [ "$(hostname -s)" = "ariel" ]; then
-    DUPLICACY="/Users/tom/bin/duplicacy"
+    # DUPLICACY="/Users/tom/bin/duplicacy"
+    backupRepository /etc
     backupRepository /Users/tom
-    #backupRepository /Volumes/RAID10/Music
-    #backupRepository /Volumes/RAID10/Photos
-elif [ "$(hostname -s)" = "gabriel" ]; then
-    DUPLICACY="/root/bin/duplicacy"
-    # gabriel_tom
-    time backupRepositoryVerbose /home/tom
-    # gabriel_root
-    time backupRepositoryVerbose /root
-    # gabriel_etc
-    time backupRepositoryVerbose /etc
-    #time backupRepository /mnt/user/Media
-    #time backupRepository /mnt/user/Music
-    #time backupRepository /mnt/user/Photos
-    #time backupRepository /mnt/user/Vault
-    #time backupRepository /mnt/user/Warehouse
-    #time copyDefault2b2 /mnt/user/Photos
+    backupRepository /Volumes/RAID10/Music
+    backupRepository /Volumes/RAID10/Photos
+elif [ "$(hostname -s)" = "theophilus" ]; then
+    # backupRepository /etc
+    # backupRepository /root
+    backupRepository /home/tom
+    # backupRepository /usr/local
+fi
+
+if [ "$(hostname -s)" = "pvhost2" ]; then
+    backupRepository /etc
+    backupRepository /root
+    backupRepository /usr/local
+    backupRepository /home/tom
+    chown -R 101000:101000 /mnt/bindmounts/duplicacy_backups
+    cd /root || exit
+
+    echo "# Copy to Backblaze..."
+    $DUPLICACY -log copy -from default -to b2 -threads 20 | tee "$DUPLICACY_LOGS/$DATETIME-copy-b2.log"
+    echo "# Done"
+
+    echo "# Copy to OneDrive..."
+    $DUPLICACY -log copy -from default -to onedrive -threads 20 | tee "$DUPLICACY_LOGS/$DATETIME-copy-onedrive.log"
+    echo "# Done"
+
+    echo "# Prune Backups..."
+    # $DUPLICACY -log prune                   -all -keep 0:360 -keep 30:180 -keep 7:30 -keep 1:7 | tee "$DUPLICACY_LOGS/$DATETIME-prune.log"
+    $DUPLICACY -log prune                   -all -keep 30:180 -keep 7:30 -keep 1:7 | tee "$DUPLICACY_LOGS/$DATETIME-prune.log"
+    $DUPLICACY -log prune -storage b2       -all -keep 30:180 -keep 7:30 -keep 1:7 | tee "$DUPLICACY_LOGS/$DATETIME-prune-b2.log"
+    $DUPLICACY -log prune -storage onedrive -all -keep 30:180 -keep 7:30 -keep 1:7 | tee "$DUPLICACY_LOGS/$DATETIME-prune-onedrive.log"
+    echo "# Done"
 fi
