85d1725f8290f62c5e84f7c2690031f767ccd2ad
Tom Hoover <tom@hisword.net>
2022-11-02 13:30:15 -0500

    Remove -f from ln commands

--- LaunchAgents/createLinks.sh
+++ LaunchAgents/createLinks.sh
@@ -2,7 +2,7 @@
 
 [[ "$(uname)" = "Darwin" ]] || exit
 
-cd "${HOME}/Library/LaunchAgents" && ln -sf ../../bin/LaunchAgents/*.plist .
+cd "${HOME}/Library/LaunchAgents" && ln -s ../../bin/LaunchAgents/*.plist .
 
 cd "${HOME}/bin/LaunchDaemons-System" && sudo chown root:wheel us.t0m.*.plist
-cd /Library/LaunchDaemons && sudo ln -sf /Users/tom/bin/LaunchDaemons-System/us.t0m.*.plist .
+cd /Library/LaunchDaemons && sudo ln -s /Users/tom/bin/LaunchDaemons-System/us.t0m.*.plist .
