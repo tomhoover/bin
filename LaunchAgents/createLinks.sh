543a146858ba4893d981c8961a076be853753cfb
Tom Hoover <tom@hisword.net>
2022-11-10 07:24:38 -0600

    cp plist LaunchDaemon plist files instead of linking

--- LaunchAgents/createLinks.sh
+++ LaunchAgents/createLinks.sh
@@ -4,5 +4,6 @@
 
 cd "${HOME}/Library/LaunchAgents" && ln -s ../../bin/LaunchAgents/*.plist .
 
-cd "${HOME}/bin/LaunchDaemons-System" && sudo chown root:wheel us.t0m.*.plist
-cd /Library/LaunchDaemons && sudo ln -s /Users/tom/bin/LaunchDaemons-System/us.t0m.*.plist .
+# cd "${HOME}/bin/LaunchDaemons-System" && sudo chown root:wheel us.t0m.*.plist
+# cd /Library/LaunchDaemons && sudo ln -s /Users/tom/bin/LaunchDaemons-System/us.t0m.*.plist .
+sudo cp /Users/tom/bin/LaunchDaemons-System/us.t0m.*.plist /Library/LaunchDaemons
