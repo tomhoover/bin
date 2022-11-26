d4c3a9939459626806b0dccb5cc39bc4dc3282ca
Tom Hoover <tom@hisword.net>
2022-11-25 20:34:46 -0600

    Add vcsh-list-tracked.sh

---
+++ vcsh-list-tracked.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+for repo in $(vcsh list)
+do
+    echo "$repo":
+    vcsh run "$repo" git log --pretty=format: --name-only | sort -u | sed '/^$/d' > ~/vcsh-"$repo"-all-files.txt
+    vcsh run "$repo" git ls-tree -r master --name-only > ~/vcsh-"$repo"-files.txt
+    diff ~/vcsh-"$repo"-all-files.txt ~/vcsh-"$repo"-files.txt
+done
