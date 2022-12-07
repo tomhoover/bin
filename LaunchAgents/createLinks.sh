e93050c198aed0c35f95e97449ace17bfc150592
Tom Hoover <tom@hisword.net>
2022-12-07 11:48:27 -0600

    Fix us.t0m.mosh-firewall.plist

--- LaunchAgents/createLinks.sh
+++ LaunchAgents/createLinks.sh
@@ -2,8 +2,20 @@
 
 [[ "$(uname)" = "Darwin" ]] || exit
 
-cd "${HOME}/Library/LaunchAgents" && ln -s ../../bin/LaunchAgents/*.plist .
+cd "${HOME}/Library/LaunchAgents" || exit
+ln -s ../../bin/LaunchAgents/*.plist . 2>/dev/null
+for f in us.t0m.*.plist
+do
+    echo "$f"
+    sudo launchctl bootstrap user/"$(id -u)/${HOME}/Library/LaunchAgents/$f"
+done
 
-# cd "${HOME}/bin/LaunchDaemons-System" && sudo chown root:wheel us.t0m.*.plist
-# cd /Library/LaunchDaemons && sudo ln -s /Users/tom/bin/LaunchDaemons-System/us.t0m.*.plist .
-sudo cp /Users/tom/bin/LaunchDaemons-System/us.t0m.*.plist /Library/LaunchDaemons
+cd /Library/LaunchDaemons || exit
+sudo cp /Users/tom/bin/LaunchDaemons-System/us.t0m.*.plist .
+for f in us.t0m.*.plist
+do
+    echo "$f"
+    sudo launchctl bootout system /Library/LaunchDaemons/"$f"
+    sudo launchctl bootstrap system /Library/LaunchDaemons/"$f"
+    sudo launchctl list |grep us.t0m
+done
