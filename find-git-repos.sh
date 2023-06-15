2a5df04050cc08914caaa38f888a79e3cda35e54
Tom Hoover <tom@hisword.net>
2023-06-14 20:51:19 -0500

    Adjust paths

--- find-git-repos.sh
+++ find-git-repos.sh
@@ -6,28 +6,27 @@
 # set -o pipefail: cause a pipeline to fail, if any command within it fails
 set -eu -o pipefail
 
-cd && find . \( -path './.Trash' -o -path './.config-backup' -o -path './.config-backup2' -o -path './Library/CloudStorage/OneDrive-Personal/.Trash' -o -path './Library/Containers' -o -path './Library/Preferences/fyne' -o -path './data' \) -prune \
+cd && find . \( -path './.config-backup' -o -path './.config-backup2' -o -path './Library/Containers' -o -path './Library/Preferences/fyne' -o -path './data' \) -prune \
     -o -type d -name '.git' | \
-    grep -v '^./.Trash' | \
-    grep -v '^./.asdf/plugins' | \
-    grep -v '^./.asdf/repository' | \
-    grep -v '^./.cache/pre-commit' | \
     grep -v '^./.config-backup' | \
     grep -v '^./.config-backup2' | \
-    grep -v '^./.config/tmux/plugins' | \
-    grep -v '^./.pyenv.old2' | \
-    grep -v '^./.pyenv.old2/plugins' | \
-    grep -v '^./.pyenv.old2/plugins' | \
-    grep -v '^./.pyenv/plugins' | \
-    grep -v '^./.tmux.old/plugins' | \
-    grep -v '^./.tmux.tch/plugins' | \
-    grep -v '^./.tmux/plugins/tpm' | \
-    grep -v '^./.vim/plugged' | \
     grep -v '^./Library/Containers' | \
     grep -v '^./Library/Preferences/fyne' | \
-    grep -v '^./Library/Preferences/fyne' | \
     grep -v '^./data' | \
-    grep -v '^./pkg/mod/cache/download/git.apache.org/thrift.git' | \
-    grep -v '^./test' | \
-    grep -v '^./tmp' | \
-    sed -e 's|^./|~/|' -e 's|/.git$||' -e 's| |\\ |g' | sort > ~/tmp/all-git-repos.txt
+    grep -v '/.stversions/' | \
+    grep -v '^./.Trash/' | \
+    grep -v '^./.asdf/' | \
+    grep -v '^./.cache/pre-commit/' | \
+    grep -v '^./.config/tmux/plugins/' | \
+    grep -v '^./.pyenv.old2/' | \
+    grep -v '^./.pyenv/' | \
+    grep -v '^./.tmux.old/plugins/' | \
+    grep -v '^./.tmux.tch/plugins/' | \
+    grep -v '^./.tmux/plugins/tpm/' | \
+    grep -v '^./.vim/plugged/' | \
+    grep -v '^./Library/Application Support/TheArchive/LocalThemeRepo/' | \
+    grep -v '^./Library/CloudStorage/OneDrive-Personal/.Trash/' | \
+    grep -v '^./pkg/mod/cache/download/git.apache.org/thrift.git/' | \
+    grep -v '^./test/' | \
+    grep -v '^./tmp/' | \
+    sed -e 's|^./|~/|' -e 's|/.git$|/|' -e 's| |\\ |g' | sort > ~/tmp/all-git-repos.txt
