7c5ff2173b9a9c3c57d6a23eb604246bf7f6353e
Tom Hoover <tom@hisword.net>
2023-06-23 10:55:50 -0500

    Ignore ~/.cache/AUR/ &  ~/.dropbox-dist/ when finding git repos

--- find-git-repos.sh
+++ find-git-repos.sh
@@ -6,16 +6,18 @@
 # set -o pipefail: cause a pipeline to fail, if any command within it fails
 set -eu -o pipefail
 
-cd && find . \( -path './.Trash' -o -path './.config-backup' -o -path './.config-backup2' -o -path './Library/Containers' -o -path './Library/Preferences/fyne' -o -path './data' \) -prune \
+cd && find . \( -path './.Trash' -o -path './.cache/AUR/sedutil.old/pkg' -o -path './.config-backup' -o -path './.config-backup2' -o -path './.dropbox-dist' -o -path './Library/Containers' -o -path './Library/Preferences/fyne' -o -path './data' \) -prune \
     -o -type d -name '.git' | \
     grep -v '^./.Trash' | \
     grep -v '^./.config-backup' | \
     grep -v '^./.config-backup2' | \
+    grep -v '^./.dropbox-dist' | \
     grep -v '^./Library/Containers' | \
     grep -v '^./Library/Preferences/fyne' | \
     grep -v '^./data' | \
     grep -v '/.stversions/' | \
     grep -v '^./.asdf/' | \
+    grep -v '^./.cache/AUR/' | \
     grep -v '^./.cache/pre-commit/' | \
     grep -v '^./.config/tmux/plugins/' | \
     grep -v '^./.pyenv.old2/' | \
@@ -27,6 +29,7 @@ cd && find . \( -path './.Trash' -o -path './.config-backup' -o -path './.config
     grep -v '^./Library/Application Support/TheArchive/LocalThemeRepo/' | \
     grep -v '^./Library/CloudStorage/OneDrive-Personal/.Trash/' | \
     grep -v '^./pkg/mod/cache/download/git.apache.org/thrift.git/' | \
+    grep -v '^./src/AUR/' | \
     grep -v '^./test/' | \
     grep -v '^./tmp/' | \
     sed -e 's|^./|~/|' -e 's|/.git$|/|' -e 's| |\\ |g' | sort > ~/tmp/all-git-repos.txt
