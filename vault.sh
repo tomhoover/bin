b173917825aff0a2a81d81bd6af1b09785e02ffc
Tom Hoover <tom@hisword.net>
2023-08-17 07:53:42 -0500

    Revise vault.sh

--- vault.sh
+++ vault.sh
@@ -3,6 +3,6 @@
 # shellcheck source=/dev/null
 [ -r "$HOME"/.keychain/"$(uname -n)"-sh ] && . "$HOME"/.keychain/"$(uname -n)"-sh
 
-vcsh vault pull 2>/dev/null 2>&1
+vcsh vault pull >/dev/null 2>&1
 vcsh vault add "*.md" 2>/dev/null
-vcsh vault commit -am "$(date '+%Y-%m-%d %H:%M')" >/dev/null 2>&1 && vcsh vault push gitea
+vcsh vault commit -am "$(date '+%Y-%m-%d %H:%M')" >/dev/null 2>&1 && vcsh vault push gitea >/dev/null 2>&1 || exit 0
