413fb34b0f5eaced5d39226e22cf67cb4a86f0d2
Tom Hoover <tom@hisword.net>
2023-10-27 17:34:41 -0500

    Nerd fonts & ligatures

---
+++ test-nerd-fonts-and-ligatures.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# echo -e "Normal"
+# echo -e "\033[1mBold\033[22m"
+# echo -e "\033[3mItalic\033[23m"
+# echo -e "\033[3;1mBold Italic\033[0m"
+# echo -e "\033[4mUnderline\033[24m"
+# echo -e "== === !== >= <= =>"
+# echo -e "契          勒 鈴 "
+
+echo
+
+smoke_tests="Normal
+\033[1mBold\033[22m
+\033[3mItalic\033[23m
+\033[3;1mBold Italic\033[0m
+\033[4mUnderline\033[24m
+== === !== >= <= =>
+󰐊     󰄉      󰑓 󰒲 "
+
+printf "%b" "${smoke_tests}"
+
+echo
+echo
