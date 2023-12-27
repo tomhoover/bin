e7e180e873cbcb62d439337e901ae3e54592f469
Tom Hoover <tom@hisword.net>
2023-12-27 12:16:07 -0600

    Copy homebrew.mxcl.minio.plist, if required, and restart minio

--- homebrewBackup_weekly.sh
+++ homebrewBackup_weekly.sh
@@ -12,3 +12,13 @@ MYHOST=$(uname -n | sed 's/\..*//')     # alternative to $(hostname -s), as arch
 
 brew bundle dump --force --file="$HOME/.homebrew-brewfile/Brewfile.${MYHOST}"
 cd "$HOME/.homebrew-brewfile" && git pull && git commit -am "${MYHOST} - $(date)" && git push origin; git push gitea
+
+if [ "$MYHOST" = "bethel" ]; then
+    if [ "$(find /opt/homebrew/Cellar/minio -name homebrew.mxcl.minio.plist)" ]; then
+       exit 0
+    fi
+    brew services stop minio
+    VERSION_DIR="$(find /opt/homebrew/Cellar/minio -type d -depth 1 | tail -1)"
+    cp ~/bin/LaunchAgents/homebrew.mxcl.minio.plist.backup "$VERSION_DIR/homebrew.mxcl.minio.plist"
+    brew services start minio
+fi
