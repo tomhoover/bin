a0d443899b6fdf63ce311747a6a4c424b3dbaa5f
Tom Hoover <tom@hisword.net>
2025-11-07 18:48:41 -0600

    modify for use with all hosts, not just bethel

--- backup-docker.sh
+++ backup-docker.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env sh
+# shellcheck disable=SC2029
 
 # set -e: exit script immediately upon error
 # set -u: treat unset variables as an error
 set -eu
 
-BACKUPDIR=/Volumes/external/backups/docker
-FILENAME=$(date +%Y%m%d-%H%M%S).tgz
+MYHOST=$(uname -n | sed 's/\..*//')     # alternative to $(hostname -s), as arch does not install 'hostname' by default
+BACKUPDIR=/Volumes/external/backups/docker/${MYHOST}
+BACKUPHOST=bethel
 
 cd "${HOME}/data/docker" || exit
 
-# shellcheck disable=SC2174
-if ! [ -d ${BACKUPDIR} ] ; then mkdir -pm 0700 ${BACKUPDIR} ; fi
-
-tar -czvf "${BACKUPDIR}/gitolite-${FILENAME}" gitolite-git gitolite-sshkeys
+# TODO: find -s . -maxdepth 1 -type d | while read -r dir; do
+#  the preceding line wasn't reading thru all dirs, so I substituted the
+#  following line for now:
+# shellcheck disable=SC2044
+for dir in $(find -s . -maxdepth 1 -type d); do
+    if [ "${dir}" = "." ]; then continue; fi
+    DATADIR=$(echo "${dir}" | sed -e 's/^\.\///')
+    FILENAME=$(date +%Y%m%d-%H%M%S)--${DATADIR}.tgz
+    ssh ${BACKUPHOST} "[ -d ${BACKUPDIR}/${DATADIR} ]" || ssh ${BACKUPHOST} "mkdir -pm 0700 ${BACKUPDIR}/${DATADIR}"
+    tar -czf - "${DATADIR}" | ssh ${BACKUPHOST} "cat > ${BACKUPDIR}/${DATADIR}/${FILENAME}"
+done
