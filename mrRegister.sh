3f9b506a534a685e66a75c8fee9fe46c9dd14b99
Tom Hoover <tom@hisword.net>
2025-11-07 21:46:56 -0600

    add ~/src/3dPrinting/.mrconfig

--- mrRegister.sh
+++ mrRegister.sh
@@ -7,48 +7,45 @@
 set -eu -o pipefail
 
 # manually check for duplicate entries:
-echo ""
+echo
 echo "*** Already registered with myrepos -- check for duplicate entries:"
 read -rsn 1 -p "               (press any key to continue)"
+clear
+echo
+echo "Searching for git repos..."
+
+cd && echo '' > ~/tmp/tmp.mrconfig && /usr/bin/grep '^\[' .mrconfig src/.mrconfig src/3dPrinting/.mrconfig src/github.com/.mrconfig .config/mr/* |sed -E 's/^(.*):(.*)/\2 ::: \1/' |sed -E 's/^#(.*)/\1#####/' |grep -v '^ ' |grep -v ^skip |grep -v ^.DEFAULT |sort |less
+
+echo "#!/bin/bash" > ~/tmp/register.sh
+echo "cd /Users/tom || exit" >> ~/tmp/register.sh
 echo ""
 
-cd && echo '' > tmp.mrconfig && /usr/bin/grep '\[' .mrconfig src/.mrconfig src/github.com/.mrconfig .config/mr/* |sed -E 's/^(.*):(.*)/\2 ::: \1/' |sed -E 's/^#(.*)/\1#####/' |grep -v '^ ' |grep -v ^skip |grep -v ^.DEFAULT |sort |less
-
-echo "#!/bin/bash" > ~/register.sh
-echo "cd /Users/tom || exit" >> ~/register.sh
-echo ""
-
-cd && find . \( -path './.config-backup' -o -path './.config-backup2' -o -path './Library/Containers' -o -path './Library/Preferences/fyne' -o -path './data' \) -prune -o -type d -name '.git' | \
-    grep -v '^./.asdf/plugins' | \
-    grep -v '^./.asdf/repository' | \
-    grep -v '^./.cache/pre-commit' | \
-    grep -v '^./.config-backup' | \
-    grep -v '^./.config-backup2' | \
-    grep -v '^./.config/tmux/plugins' | \
-    grep -v '^./.tmux.old/plugins' | \
-    grep -v '^./.tmux.tch/plugins' | \
-    grep -v '^./.vim/plugged' | \
-    grep -v '^./Library/Containers' | \
-    grep -v '^./data' | \
-    grep -v '^./test' | \
-    grep -v '^./tmp' | \
+cd && time find . \( -path './Library' -o -path './data' -o -path './dl' -o -path './doc' -o -path './tmp' \) -prune -o -type d -name '.git' | \
+    grep -v '^./Library$' | \
+    grep -v '^./data$' | \
+    grep -v '^./dl$' | \
+    grep -v '^./doc$' | \
+    grep -v '^./tmp$' | \
+    grep -v '^./.ansible/roles/trfore.omada_install/' | \
+    grep -v '^./.cache/cookiecutters/' | \
+    grep -v '^./.cache/pre-commit/' | \
+    grep -v '^./.local/share/nvim/lazy/' | \
+    grep -v '^./.config/tmux/plugins/' | \
+    grep -v '^./.vim/plugged/' | \
     sed -e 's|^./||' -e 's|/.git$||' | while read -r REPOSITORY; do
-    grep -F "${REPOSITORY}]" ~/.mrconfig ~/src/.mrconfig ~/src/github.com/.mrconfig ~/.config/mr/* || echo "mr -c ~/tmp.mrconfig register $REPOSITORY" >> ~/register.sh
+    grep -F "${REPOSITORY}]" ~/.mrconfig ~/src/.mrconfig ~/src/3dPrinting/.mrconfig ~/src/github.com/.mrconfig ~/.config/mr/* || echo "mr -c ~/tmp/tmp.mrconfig register '${REPOSITORY}'" >> ~/tmp/register.sh
 done
 
 {
-    echo "sed -e '/^\[\$HOME/! s/^\[/[\$HOME\//g' -e 's/^\[/~[/g' tmp.mrconfig |tr '\n' '^' |tr '~' '\n' |sort |tr -s '^' |tr '^' '\n' > tmp2.mrconfig"
-    echo "mv tmp2.mrconfig tmp.mrconfig"
+    echo "sed -e 's/\/home\/tom\///' -e 's/\/Users\/tom\///' -e '/^\[\$HOME/! s/^\[/[\$HOME\//g' -e 's/^\[/~[/g' ~/tmp/tmp.mrconfig |tr '\n' '^' |tr '~' '\n' |sort |tr -s '^' |tr '^' '\n' > ~/tmp/tmp2.mrconfig"
+    echo "mv ~/tmp/tmp2.mrconfig ~/tmp/tmp.mrconfig"
     echo "echo ''"
     echo "echo 'finally, edit the following to taste:'"
-    # echo 'echo "     vi ~/tmp.mrconfig ~/.mrconfig ~/src/.mrconfig ~/src/github.com/.mrconfig ~/data/software/github.com/.mrconfig ~/.config/mr/*"'
-    echo "echo '     vi ~/tmp.mrconfig ~/.mrconfig ~/src/.mrconfig ~/src/github.com/.mrconfig ~/.config/mr/*'"
+    echo "echo '     vi ~/tmp/tmp.mrconfig ~/.mrconfig ~/src/.mrconfig ~/src/3dPrinting/.mrconfig ~/src/github.com/.mrconfig ~/.config/mr/*'"
     echo "echo ''"
-    echo "rm -f /Users/tom/register.sh"
-} >> ~/register.sh
+    echo "rm -f /Users/tom/tmp/register.sh"
+} >> ~/tmp/register.sh
 
-chmod +x ~/register.sh
-
-echo ""
-echo "edit, and then execute, ~/register.sh"
-echo ""
+chmod +x ~/tmp/register.sh
+~/tmp/register.sh
+vi ~/tmp/tmp.mrconfig ~/.mrconfig ~/src/.mrconfig ~/src/3dPrinting/.mrconfig ~/src/github.com/.mrconfig ~/.config/mr/*
