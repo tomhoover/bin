50ac011fc836376ee52d7bcb6a5b7353a5a6983a
Tom Hoover <tom@hisword.net>
2026-07-17 16:50:39 -0500

    update colors.sh

--- colors.sh
+++ colors.sh
@@ -1,15 +1,22 @@
-# shellcheck shell=sh
-# shellcheck disable=SC2034
+# shellcheck shell=sh disable=SC2034
 
-# Only use colors in interactive shells, not from crontab
-if [ -t 1 ]; then
-    RED=$(tput setaf 1)
-    GREEN=$(tput setaf 2)
-    YELLOW=$(tput setaf 3)
-    BLUE=$(tput setaf 4)
-    MAGENTA=$(tput setaf 5)
-    CYAN=$(tput setaf 6)
-    RESET=$(tput sgr0)
+# Only use colors in interactive shells, not from crontab; honor NO_COLOR (https://no-color.org) #{{{
+if [ -t 1 ] && [ -z "${NO_COLOR:-}" ]; then
+    RED=$(tput setaf 1 2>/dev/null || true)
+    GREEN=$(tput setaf 2 2>/dev/null || true)
+    YELLOW=$(tput setaf 3 2>/dev/null || true)
+    BLUE=$(tput setaf 4 2>/dev/null || true)
+    MAGENTA=$(tput setaf 5 2>/dev/null || true)
+    CYAN=$(tput setaf 6 2>/dev/null || true)
+    BOLD=$(tput bold 2>/dev/null || true)
+    DIM=$(tput dim 2>/dev/null || true)
+    STANDOUT_ON=$(tput smso 2>/dev/null || true)
+    STANDOUT_OFF=$(tput rmso 2>/dev/null || tput sgr0 2>/dev/null || true)
+    UNDERLINE_ON=$(tput smul 2>/dev/null || true)
+    UNDERLINE_OFF=$(tput rmul 2>/dev/null || tput sgr0 2>/dev/null || true)
+    ITALICS_ON=$(tput sitm 2>/dev/null || true)
+    ITALICS_OFF=$(tput ritm 2>/dev/null || tput sgr0 2>/dev/null || true)
+    RESET=$(tput sgr0 2>/dev/null || true)
 else
     RED=
     GREEN=
@@ -17,6 +24,14 @@ else
     BLUE=
     MAGENTA=
     CYAN=
+    BOLD=
+    DIM=
+    STANDOUT_ON=
+    STANDOUT_OFF=
+    UNDERLINE_ON=
+    UNDERLINE_OFF=
+    ITALICS_ON=
+    ITALICS_OFF=
     RESET=
 fi
 
@@ -24,3 +39,19 @@ fi
 #   # shellcheck source=/dev/null
 #   source ~/bin/colors.sh
 #   echo "${RED}ABORTED!${RESET} ${REPOSITORY} does not exist!"
+
+# echo "${RED}RED"
+# echo "${GREEN}GREEN"
+# echo "${YELLOW}YELLOW"
+# echo "${BLUE}BLUE"
+# echo "${MAGENTA}MAGENTA"
+# echo "${CYAN}CYAN"
+# echo "${BOLD}BOLD"
+# echo "${DIM}DIM${RESET}"
+# echo "${STANDOUT_ON}STANDOUT_ON"
+# echo "${STANDOUT_OFF}STANDOUT_OFF"
+# echo "${UNDERLINE_ON}UNDERLINE_ON"
+# echo "${UNDERLINE_OFF}UNDERLINE_OFF"
+# echo "${ITALICS_ON}ITALICS_ON"
+# echo "${ITALICS_OFF}ITALICS_OFF"
+# echo "${RESET}RESET"
